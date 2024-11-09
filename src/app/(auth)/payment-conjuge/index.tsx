a577cac21b942212ddcabc69d1c487693ac1c0c7
unknown <2003silvagui@gmail.com>
2024-11-09 11:43:30 -0300

    feat: payment conjuge

--- src/app/(auth)/payment-conjuge/index.tsx
+++ src/app/(auth)/payment-conjuge/index.tsx
@@ -2,11 +2,9 @@ import {
   Box,
   Button,
   Center,
-  CheckIcon,
   FormControl,
   HStack,
   Heading,
-  Select,
   Text,
   VStack,
 } from "native-base";
@@ -14,19 +12,20 @@ import { colors } from "../../../theme/colors";
 import { Alert, ScrollView } from "react-native";
 import { useGlobalContext } from "../../../context/context";
 import { MaterialIcons } from "@expo/vector-icons";
-import { screens } from "../../../mock/screens";
-import { parcelas } from "../../../mock/parcelas";
 import React, { useEffect } from "react";
 import Footer from "../../../components/Footer/Footer";
-import { User, UserData } from "../../../utils/user";
+import { User } from "../../../utils/user";
 import Toast from "react-native-toast-message";
 import LoadingTransparent from "../../../components/LoadingTransparent/LoadingTransparent";
 import usePayment from "../../../hooks/usePayment";
-import { CardField, confirmPayment } from "@stripe/stripe-react-native";
+import {
+  CardField,
+  confirmPayment,
+  StripeProvider,
+} from "@stripe/stripe-react-native";
 import { router } from "expo-router";
 
 export default function PaymentScreenConjuge() {
-  const [service, setService] = React.useState("");
   const { navigation, setIsLoading, isLoading } = useGlobalContext();
 
   const { intentPayment } = usePayment();
@@ -55,120 +54,128 @@ export default function PaymentScreenConjuge() {
     }
   };
 
-  const [selectedValue, setSelectedValue] = React.useState(2);
-
   return (
     <>
       {isLoading && <LoadingTransparent />}
       <ScrollView style={{ backgroundColor: colors.background }}>
-        <Center
-          w="100%"
-          h="100%"
-          pb={100}
-          style={{ backgroundColor: colors.background }}
+        <StripeProvider
+          publishableKey={process.env.EXPO_PUBLIC_STRIPE_KEY_PUBLISH as string}
         >
-          <VStack
-            backgroundColor={colors.background}
-            h="full"
-            w="full"
-            maxW={350}
+          <Center
+            w="100%"
+            h="100%"
+            pb={100}
+            style={{ backgroundColor: colors.background }}
           >
-            <Box mt={7}>
-              <Heading fontFamily="PathwayBold" fontSize={32}>
-                Validação de {"\n"}pagamento
-              </Heading>
-            </Box>
-            <Text fontFamily="PathwayRegular" fontSize={16} mt={2}>
-              Ao fim do processo será cobrado o valor de R$ 4.000,00
-            </Text>
-
-            <HStack
-              mt={7}
-              alignItems={"center"}
-              justifyContent={"space-between"}
+            <VStack
+              backgroundColor={colors.background}
+              h="full"
+              w="full"
+              maxW={350}
             >
-              <Heading fontFamily="PathwayBold" fontSize={18}>
-                SUA PARTE DO {"\n"}PAGAMENTO É DE {user.pagamento?.porcentagem}%
-              </Heading>
-              <Heading fontFamily="PathwayBold" fontSize={24}>
-                {user.pagamento?.valor_pago &&
-                  user.pagamento?.valor_pago.toLocaleString("pt-BR", {
-                    style: "currency",
-                    currency: "brl",
-                  })}
-              </Heading>
-            </HStack>
+              <Box mt={7}>
+                <Heading fontFamily="PathwayBold" fontSize={32}>
+                  Validação de {"\n"}pagamento
+                </Heading>
+              </Box>
+              <Text fontFamily="PathwayRegular" fontSize={16} mt={2}>
+                Ao fim do processo será cobrado o valor de R$ 4.000,00
+              </Text>
 
-            <VStack mt={4} space={2}>
-              <FormControl>
-                <FormControl.Label>Cartão</FormControl.Label>
-                <CardField
-                  postalCodeEnabled={false}
-                  placeholders={{
-                    number: "4242 4242 4242 4242",
-                  }}
-                  cardStyle={{
-                    backgroundColor: "#FFFFFF",
-                    textColor: "#1f1c1c",
-                    borderWidth: 1,
-                    borderColor: "#BECDCF",
-                    borderRadius: 8,
-                  }}
-                  style={{
-                    width: "100%",
-                    height: 50,
-                    marginVertical: 4,
-                  }}
-                />
-              </FormControl>
-            </VStack>
-            <Box alignItems="flex-end" w="100%" mt={10}>
-              <Button
-                onPress={async () => {
-                  try {
-                    setIsLoading(true);
-                    const { paymentIntent: intentKey } = await intentPayment(
-                      Number(user.pagamento?.porcentagem)
-                    );
-
-                    const { paymentIntent, error } = await confirmPayment(
-                      intentKey,
-                      {
-                        paymentMethodType: "Card",
-                      }
-                    );
-                    if (paymentIntent) {
-                      Toast.show({
-                        text1: "Pagamento confirmado!",
-                      });
-                      router.push("/upload");
-                    }
-                    if (error) {
-                      Alert.alert(
-                        "Verifique as informações e tente novamente!"
-                      );
-                    }
-                  } finally {
-                    setIsLoading(false);
-                  }
-                }}
-                backgroundColor={colors.yellow}
-                h={52}
-                px={"4"}
-                rounded="2xl"
-                textDecorationColor="black"
-                flexDirection="row"
-                endIcon={
-                  <MaterialIcons name="navigate-next" size={24} color="black" />
-                }
+              <HStack
+                mt={7}
+                alignItems={"center"}
+                justifyContent={"space-between"}
               >
-                <Text fontSize={16} fontFamily="PathwayBold">
-                  Salvar e Proximo
-                </Text>
-              </Button>
-            </Box>
-          </VStack>
-        </Center>
+                <Heading fontFamily="PathwayBold" fontSize={18}>
+                  SUA PARTE DO {"\n"}PAGAMENTO É DE{" "}
+                  {user.pagamento?.porcentagem}%
+                </Heading>
+                <Heading fontFamily="PathwayBold" fontSize={24}>
+                  {user.pagamento?.valor_pago &&
+                    user.pagamento?.valor_pago.toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "brl",
+                    })}
+                </Heading>
+              </HStack>
+
+              <VStack mt={4} space={2}>
+                <FormControl>
+                  <FormControl.Label>Cartão</FormControl.Label>
+                  <CardField
+                    postalCodeEnabled={false}
+                    placeholders={{
+                      number: "4242 4242 4242 4242",
+                    }}
+                    cardStyle={{
+                      backgroundColor: "#FFFFFF",
+                      textColor: "#1f1c1c",
+                      borderWidth: 1,
+                      borderColor: "#BECDCF",
+                      borderRadius: 8,
+                    }}
+                    style={{
+                      width: "100%",
+                      height: 50,
+                      marginVertical: 4,
+                    }}
+                  />
+                </FormControl>
+              </VStack>
+              <Box alignItems="flex-end" w="100%" mt={10}>
+                <Button
+                  onPress={async () => {
+                    try {
+                      setIsLoading(true);
+                      console.log(process.env.STRIPE_KEY_PUBLISH as string)
+                      const { paymentIntent: intentKey } = await intentPayment(
+                        Number(user.pagamento?.porcentagem)
+                      );
+
+                      const { paymentIntent, error } = await confirmPayment(
+                        intentKey,
+                        {
+                          paymentMethodType: "Card",
+                        }
+                      );
+                      if (paymentIntent) {
+                        Toast.show({
+                          text1: "Pagamento confirmado!",
+                        });
+                        router.push("/upload");
+                      }
+                      if (error) {
+                        Alert.alert(
+                          "Verifique as informações e tente novamente!"
+                        );
+                      }
+                    } finally {
+                      setIsLoading(false);
+                    }
+                  }}
+                  backgroundColor={colors.yellow}
+                  h={52}
+                  px={"4"}
+                  rounded="2xl"
+                  textDecorationColor="black"
+                  flexDirection="row"
+                  endIcon={
+                    <MaterialIcons
+                      name="navigate-next"
+                      size={24}
+                      color="black"
+                    />
+                  }
+                >
+                  <Text fontSize={16} fontFamily="PathwayBold">
+                    Salvar e Proximo
+                  </Text>
+                </Button>
+              </Box>
+            </VStack>
+          </Center>
+        </StripeProvider>
       </ScrollView>
       <Footer />
     </>
